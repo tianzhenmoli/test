f811449f138fefb24cc82dba2f06f3d4ead3383f
wangyongcun <yongcunwang@qq.com>
2018-05-24 10:27:15 +0800

    添加1.py功能模块之MyClass类

---
+++ 1.py
@@ -0,0 +1,21 @@
+class MyClass(object):
+	author = 'wangyongcun'
+	
+	def __init__(self, name, age):
+		self.name = name
+		self.age = age
+		
+	def func(self):
+		print(self.name, self.age)
+		
+	@classmethod
+	def class_func(cls):
+	    print('class_func')
+		
+	@staticmethod
+	def static_func():
+	    print('static_func')
+		
+	@property
+	def property_func(self):
+	    print('property)
